19c47d0b1fc49ada614c3ea98968750971526211
Youjinwoo <104017657+Youjinwoo@users.noreply.github.com>
2022-04-23 00:32:57 +0900

    수정했ㅇㅇㅇ

--- app/src/main/java/com/example/androidlab/MainActivity.kt
+++ app/src/main/java/com/example/androidlab/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //어버버버
+        //어버버버2
     }
 }
