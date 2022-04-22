29b2c4f23529cdd981b5ea60fd1949f16786a916
kerisis <87815544+kerisis@users.noreply.github.com>
2022-04-23 00:08:13 +0900

    leece  04.23 12:08

--- app/src/main/java/com/example/androidlab/MainActivity.kt
+++ app/src/main/java/com/example/androidlab/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //어버버버
+        
+        //으어어어어어어 github 쓰기 힘드네
     }
 }
