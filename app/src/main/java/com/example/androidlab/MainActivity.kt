bcbdd53da6d0cee44db47738a70f3cb9340ba50b
Youjinwoo <104017657+Youjinwoo@users.noreply.github.com>
2022-04-23 00:39:28 +0900

    Revert "수정했ㅇㅇㅇ"
    This reverts commit 19c47d0b1fc49ada614c3ea98968750971526211.

--- app/src/main/java/com/example/androidlab/MainActivity.kt
+++ app/src/main/java/com/example/androidlab/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //어버버버2
+        //어버버버
     }
 }
